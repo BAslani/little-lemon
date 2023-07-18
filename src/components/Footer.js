3cb962800e5102f9f9ca4ded42747ede9d7431f6
Behzad Aslani <behzadaslani80@gmail.com>
2023-07-18 22:15:42 +0330

    setting up semantic tags and meta tags and opengraph protocol

---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="navigation">
+                <h3>Navigation</h3>
+                <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#menu">Menu</a>
+                </li>
+                <li>
+                    <a href="#Reservations">Reservations</a>
+                </li>
+                <li>
+                    <a href="#orderOnline">Order Online</a>
+                </li>
+                <li>
+                    <a href="#login">Login</a>
+                </li>
+                </ul>
+            </div>
+            <div className="contacts">
+                <h3>Contacts</h3>
+                <ul>
+                    <li>Email</li>
+                    <li>Phone number</li>
+                    <li>Address</li>
+                </ul>
+            </div>
+            <div className="socialMedia">
+                <h3>Contacts</h3>
+                <ul>
+                    <li>
+                        <a href="https://www.instagram.com/">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com/r">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="https://telegram.org/">Telegram</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from "./Assets/Logo .svg"
+
+function Header() {
+    return (
+        <header>
+            <img src={Logo} alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
